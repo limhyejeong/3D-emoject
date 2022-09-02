0d8ee684137809948998a51b400e7e037678b596
limteractive <limteractive@gmail.com>
2022-09-02 21:35:10 +0900

    Remove : 사용하지 않는 모듈 제거

--- src/main.js
+++ src/main.js
@@ -1,19 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { TroisJSVuePlugin } from 'troisjs';
 import { createPinia } from 'pinia';
-// import mitt from 'mitt';
 import jQuery from 'jquery';
 
 const pinia = createPinia();
-// const emitter = mitt();
 const app = createApp(App);
-// app.config.globalProperties.$emitter = emitter
 
 app.use(pinia);
 app.use(router);
 app.use(jQuery);
-app.use(TroisJSVuePlugin);
-// app.use(mitt);
 app.mount('#app')
