f9dc1cf835bd0e82879ad060a6eee33f5145149e
limteractive <limteractive@gmail.com>
2022-12-16 11:50:28 +0900

    Refactor : 두번째 시도부터 걸리는 렉 수정

--- src/stores/input.js
+++ src/stores/input.js
@@ -35,7 +35,8 @@ export const useInputStore = defineStore('input', {
             addDoc(emoColRef, array).then(() => {
                 alert("감정이 등록되었어요!\n메인 화면 한 가운데에 띄워질 거예요.");
                 this.isSave = false;
-                router.replace("/");
+                // router.replace("/");
+                window.location.href = "/";
             });
         },
         clearInput() {
